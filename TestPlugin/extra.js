1904ae51c13cd044d23247369f4a24c931d1e2f2
Mime <109782674+xMimiez@users.noreply.github.com>
2026-09-08 01:47:46 -0500

    Add TestPlugin/extra.js

---
+++ TestPlugin/extra.js
@@ -0,0 +1,8 @@
+var TestPluginExtra = {
+    file: "extra.js",
+    token: "EXTRA_FILE_OK",
+    greet: function (name) {
+        return "hello " + name + " from extra.js";
+    }
+};
+TestPluginExtra;
